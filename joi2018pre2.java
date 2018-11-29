451f8a3a8062de03a971f9b24a85ccb3e7ea8cce
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-11-29 22:33:01 +0900

    [Update] JOI

---
+++ joi2018pre2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class joi2018pre2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // 整数の入力
+        int n = sc.nextInt();
+        int[] insert;
+        insert = new int[];
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int ans;
+
+        if(a == 1){
+ 
+        
+        // 出力
+        System.out.println((a+b+c) + " " + s);
+    }
+}
